04af6d4d1bc8b41b60a2103bef76ee0c17712b14
Victoria <vicktoria.sin@yandex.ru>
2026-01-11 01:25:13 +0300

    add postconstruct to grpc server

--- src/main/java/ru/sinvic/server/grpc/GRPCServer.java
+++ src/main/java/ru/sinvic/server/grpc/GRPCServer.java
@@ -2,18 +2,37 @@ package ru.sinvic.server.grpc;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import jakarta.annotation.PostConstruct;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.concurrent.Executors;
 
+@Component
 public class GRPCServer {
     private static final Logger logger = LoggerFactory.getLogger("GRPCServer");
 
     public static final int SERVER_PORT = 8190;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
+        new GRPCServer().startGrpcServer();
+    }
+
+    @PostConstruct
+    private void startGrpcServer() {
+        new Thread(() -> {
+            try {
+                run();
+            } catch (IOException | InterruptedException e) {
+                logger.error(e.getMessage());
+            }
+        }).start();
+    }
+
+    public void run() throws IOException, InterruptedException {
         MessageServiceImpl messageService = new MessageServiceImpl();
         Server server = ServerBuilder
             .forPort(SERVER_PORT)
